0baf845b702f4b1d80bfd44cf786e352bac06a05
Denis Golubovskii <denisgolubovskiiemail@gmail.com>
2020-05-25 12:38:27 +0300

    repaired slider

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,35 +1,39 @@
-$(document).ready(function(){
-	$('.section-testimonials__slider').slick();
+$(document).ready(function () {
+  $(".section-testimonials__slider").slick();
 });
-
 
 /*===============section-my-bread slider=================*/
 
-$(document).ready(function() { // Ждём загрузки страницы
-	$(".flexblock-3columns__background-block").click(function(){	// Событие клика на нужный блок
-	  	var img = $(this).siblings(".flexblock-3columns__image");	// Получаем изображение, на которое кликнули
-			var src = img.attr('src'); // Достаем из этого изображения путь до картинки
-			$("body").append("<div class='popup'>"+ //Добавляем в тело документа разметку всплывающего окна
-						 					 "<div class='popup_bg'></div>"+ // Блок, который будет служить фоном затемненным
-											 "<img src='"+src+"' class='popup_img' />"+ // Само увеличенное фото
-											 "</div>");
-			$(".popup").fadeIn(1000); // Медленно выводим изображение
-			$(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
-				$(".popup").fadeOut(500);	// Медленно убираем всплывающее окн	
-      	setTimeout(function() {	// Выставляем таймер
-			  	$(".popup").remove(); // Удаляем разметку всплывающего окна
-				}, 200);
-		});
-	});	
-});
-
-
-
-$(function(){
-	$("a[href^='#']").click(function(){
-  	var _href = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(_href).offset().top+"px"}, 1000);
-    return false;
+$(document).ready(function () {
+  // Ждём загрузки страницы
+  $(".flexblock-3columns__background-block").click(function () {
+    // Событие клика на нужный блок
+    var img = $(this).siblings(".flexblock-3columns__image"); // Получаем изображение, на которое кликнули
+    var src = img.attr("src"); // Достаем из этого изображения путь до картинки
+    $("body").append(
+      "<div class='popup'>" + //Добавляем в тело документа разметку всплывающего окна
+      "<div class='popup_bg'></div>" + // Блок, который будет служить фоном затемненным
+      "<img src='" +
+      src +
+      "' class='popup_img' />" + // Само увеличенное фото
+        "</div>"
+    );
+    $(".popup").fadeIn(1000); // Медленно выводим изображение
+    $(".popup_bg").click(function () {
+      // Событие клика на затемненный фон
+      $(".popup").fadeOut(500); // Медленно убираем всплывающее окн
+      setTimeout(function () {
+        // Выставляем таймер
+        $(".popup").remove(); // Удаляем разметку всплывающего окна
+      }, 200);
+    });
   });
 });
 
+$(function () {
+  $("a[href^='#']").click(function () {
+    var _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" }, 1000);
+    return false;
+  });
+});
